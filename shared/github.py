24ac37b60c6774e075a17a2b5e21d052867b9ebc
zmyapps <zmyapps>
2026-05-04 14:33:27 +0700

    combine GitHubRepo class

--- shared/github.py
+++ shared/github.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+from typing import Literal, overload
 
 from githubkit import GitHub
 from githubkit.auth import ActionAuthStrategy, TokenAuthStrategy
@@ -23,3 +24,60 @@ def get_github_client():
 
 
 github_client = get_github_client()
+
+
+class GitHubRepo:
+    @overload
+    def __init__(self, owner_or_full: str): ...
+
+    @overload
+    def __init__(self, owner_or_full: str, repo: str): ...
+
+    def __init__(self, owner_or_full: str, repo: str | None = None):
+        if repo is None:
+            self.owner, self.repo = owner_or_full.split("/")
+        else:
+            self.owner = owner_or_full
+            self.repo = repo
+
+    @overload
+    def get_release_asset_url(
+        self,
+        latest: Literal[True],
+        tag: None = None,
+        asset_name_includes: str | None = None,
+    ) -> str: ...
+
+    @overload
+    def get_release_asset_url(
+        self,
+        latest: Literal[False] | None = None,
+        tag: str = ...,
+        asset_name_includes: str | None = None,
+    ) -> str: ...
+
+    def get_release_asset_url(
+        self,
+        latest: bool | None = False,
+        tag: str | None = None,
+        asset_name_includes: str | None = None,
+    ) -> str:
+        release = None
+        if latest:
+            release = github_client.rest.repos.get_latest_release(
+                owner=self.owner, repo=self.repo
+            )
+        elif tag:
+            release = github_client.rest.repos.get_release_by_tag(
+                owner=self.owner, repo=self.repo, tag=tag
+            )
+
+        if release:
+            assets = release.parsed_data.assets
+            if asset_name_includes:
+                for asset in assets:
+                    if asset_name_includes in asset.name:
+                        return asset.browser_download_url
+            return assets[0].browser_download_url
+        else:
+            raise Exception("No release found")
