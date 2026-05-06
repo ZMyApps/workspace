5563cb6de8c9f0805a51a7f0e74ae46b4511c028
zmyapps <zmyapps>
2026-05-06 22:15:23 +0700

    print run url

--- shared/dispatch.py
+++ shared/dispatch.py
@@ -3,9 +3,10 @@ from shared.github import github_client
 
 
 def dispatch_json():
-    github_client.rest.actions.create_workflow_dispatch(
+    result = github_client.rest.actions.create_workflow_dispatch(
         owner=workspace_repo.owner,
         repo=workspace_repo.repo,
         workflow_id="json.yml",
         ref="main",
     )
+    print(result.parsed_data.html_url)
